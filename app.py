d4c5fe1dccf5f95449e59f425d31188d5717dd20
Aniket Mishra <82095877+AnIkeT126@users.noreply.github.com>
2024-02-22 23:01:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def download_text(text):
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
